383b65f11920fb371994745e52088170d296dbd3
Subhi YD <subhy809fr@gmail.com>
2022-03-15 00:26:55 +0100

    V1.4 Product to Entity, Rajout de Dépendance-JPA et -H2, Configuration application.properties, Créé de data.sql et schema.sql

--- src/main/java/com/ecommerce/microcommerce/model/Product.java
+++ src/main/java/com/ecommerce/microcommerce/model/Product.java
@@ -1,16 +1,27 @@
 package com.ecommerce.microcommerce.model;
 import com.fasterxml.jackson.annotation.JsonFilter;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 ///Elle demande à ce que prixAchat et id ne soient pas renvoyés en cas de requête pour récupérer l'entité/le Bean correspondant.
 //@JsonIgnoreProperties(value = "prixAchat", "id")
-@JsonFilter("monFiltreDynamique")
+//@JsonFilter("monFiltreDynamique")
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue //(strategy = GenerationType.IDENTITY)
     private int id;
     private String nom;
     private int prix;
 
     //information que nous ne souhaitons pas exposer
     private int prixAchat;
+
+    public Product() {
+
+    }
 
     public int getId() {
         return id;
