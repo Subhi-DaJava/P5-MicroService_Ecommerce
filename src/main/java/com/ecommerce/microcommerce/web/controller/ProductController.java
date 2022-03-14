e7000e4c62e336a3f757c920e397c2f06f70ab8b
Subhi YD <subhy809fr@gmail.com>
2022-03-14 20:40:36 +0100

    V1.2.2 Modifier @PostMapping pour respecter les conventions et les standards(201 created et location: URI)

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -2,9 +2,13 @@ package com.ecommerce.microcommerce.web.controller;
 
 import com.ecommerce.microcommerce.dao.ProductDAO;
 import com.ecommerce.microcommerce.model.Product;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 public class ProductController {
@@ -27,16 +31,33 @@ public class ProductController {
 
     /**
      * URI "/Produits/{id}", renvoyer un produit au format JSON qui correspond à la classe Product.
-     * @param id
-     * @return
      */
     //Récupérer un produit par son Id
     @GetMapping(value = "/Produits/{id}")
     public Product afficherUnProduit(@PathVariable int id){
         return productDAO.findById(id);
     }
-    @PostMapping("/Produits")
-    public void ajouteProduit(@RequestBody Product product) {
-        productDAO.save(product);
+
+    /**
+     *Dans un premier temps, nous faisons appel à la DAO pour ajouter le produit. Dans le cas où le produit ajouté est vide ou n'existe pas, nous retournons le code 204 No Content.
+     *  Pour cela, la méthode noContent() est utilisée. Cette méthode est chaînée avec la méthode build() qui construit le header, et y ajoute le code choisi.
+     * Dans le cas où tout s'est bien passé et que productAdded n'est donc pas null, nous avons besoin, en plus du code 201, d'ajouter l'URI vers cette nouvelle ressource créée,
+     * afin d'être conformes au protocole HTTP.
+     * Nous déclarons donc une instance de la classe URI afin de la passer ensuite comme argument de ResponseEntity. Nous instancions cette URI à partir de l'URL de la requête reçue.
+     * Nous ajoutons ensuite l'Id du produit à l'URI à l'aide de la méthode buildAndExpand. Nous retrouvons l'Id dans l'instance de Product que nous avons reçu : productAdded.getId().
+     * Enfin, nous invoquons la méthode created de ResponseEntity, qui accepte comme argument l'URI de la ressource nouvellement créée, et renvoie le code de statut 201.
+     */
+    @PostMapping(value = "/Produits")
+    public ResponseEntity<Product> ajouterProduit(@RequestBody Product product) {
+        Product productAdded = productDAO.save(product);
+        if (Objects.isNull(productAdded)) {
+            return ResponseEntity.noContent().build();
+        }
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(productAdded.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
