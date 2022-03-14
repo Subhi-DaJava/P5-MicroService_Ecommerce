9016de03f98ba8d1a7ea8b525b17f86164a7335e
Subhi YD <subhy809fr@gmail.com>
2022-03-14 13:28:31 +0100

    V1.1.1 créer la méthode qui envoie la requête pour afficher un produit

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -1,12 +1,17 @@
 package com.ecommerce.microcommerce.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
     @GetMapping("/Produits")
-    public String getProduit(){
+    public String listeProduits(){
        return "Un exemple de produits";
+    }
+    @GetMapping("Produits/{id}")
+    public String afficherUnProduit(@PathVariable int id){
+        return "Vous avez un produit avec l'id "+id;
     }
 }
